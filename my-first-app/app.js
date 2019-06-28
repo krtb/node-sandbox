3462fe78e79200834cf1f5b40932aa8d7d645fed
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-28 15:37:41 -0400

    add notes on export function and single objects

--- my-first-app/app.js
+++ my-first-app/app.js
@@ -1,1 +1,4 @@
-console.log(module)
+//using a constant instead of a variable, allows for tools like jshint to help
+const logger = require('./logger.js');
+
+logger.log('message')
---
+++ my-first-app/logger.js
@@ -0,0 +1,13 @@
+// not a real site
+var url = 'https://mylogger.io/log'
+
+function log(message) {
+    //send an http request
+    console.log(message)
+}
+
+//both variable and function are private, scoped to this module and not visible from the outside
+// simply adding a method the export object, found when yo console.log(module)
+module.export.log = log;
+// can also change the name of the variable that we want to export outside as well
+// module.export.endPoint = url;
