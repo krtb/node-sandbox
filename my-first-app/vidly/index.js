602058cd4f6cf8c68094273a05667b96dd81125b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-12 16:42:22 -0400

    add array of objects, contain different genres

--- my-first-app/vidly/index.js
+++ my-first-app/vidly/index.js
@@ -3,3 +3,9 @@ const express = require('express');
 const app = express()
 
 app.use(express.json())
+
+const genres = [
+    {id: 1, name: "Horror"},
+    {id: 2, name: "Comedy"},
+    {id: 3, name: "Action"},
+]
