34aeddd521648fb834128937ebe4b41f099eb06e
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-14 12:58:42 -0400

    create new app, get all genres, get single genre

--- my-first-app/vidly/index.js
+++ my-first-app/vidly/index.js
@@ -1,15 +1,37 @@
 const Joi = require('@hapi/joi');
 const express = require('express');
-const app = express()
 
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => {
+    console.log(`listening on port ${port}...`);
+})
+
 
 const genres = [
-    {id: 1, name: "Horror"},
-    {id: 2, name: "Comedy"},
-    {id: 3, name: "Action"},
-]
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Horror' },
+    { id: 3, name: 'Romance' },
+];
 
-app.get('/app/genres', (req, res)=>{
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+    //specify route and callback func, or route handler
+});
+
+//GET all genres
+app.get('/api/genres', (req,res) => {
     res.send(genres)
 })
+
+//GET a single genre
+app.get('/api/genres/:id', (req, res) => {
+    // console.log(req.params); === { id: 1}   
+    const genre = genres.find((g)=> g.id === parseInt(req.params.id) )
+    if(!genre) res.status(404).send('That genre ID could not be found.')
+    res.send(genre)
+})
+
+//CREATE/POST a new genre
