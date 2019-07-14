2daa151ab43a1e4f10bca7a635c2c91d3e0ea597
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-14 13:18:02 -0400

    add JOI func for data validation, get a single genre

--- my-first-app/vidly/index.js
+++ my-first-app/vidly/index.js
@@ -16,6 +16,12 @@ const genres = [
     { id: 3, name: 'Romance' },
 ];
 
+//middleware(JOI), used for data validation
+function validateGenre(genre) {
+    const schema = {name: Joi.string().min(3).required()}
+    return Joi.validate(genre, schema)
+}
+
 app.get('/', (req, res) => {
     res.send('Hello, World!')
     //specify route and callback func, or route handler
@@ -28,10 +34,16 @@ app.get('/api/genres', (req,res) => {
 
 //GET a single genre
 app.get('/api/genres/:id', (req, res) => {
-    // console.log(req.params); === { id: 1}   
-    const genre = genres.find((g)=> g.id === parseInt(req.params.id) )
-    if(!genre) res.status(404).send('That genre ID could not be found.')
-    res.send(genre)
+    // console.log(req.params); === { id: 1}       
+    const genre = genres.find((g)=> g.id === parseInt(req.params.id) );
+    if(!genre) res.status(404).send('That genre ID could not be found.');
+    res.send(genre);
 })
 
 //CREATE/POST a new genre
+app.post('/api/genres/:id', (req, res) => {
+    //by default this would be `undefined`, unless using body parsing middleware    
+    // const { error } = validateGenre(req.body);
+    console.log(req.body, 'HERE IS THE BODY');
+    
+})
