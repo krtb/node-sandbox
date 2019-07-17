0ddee518ff014b6faa4f7da71a5cd827cfbc3e41
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-17 19:07:34 -0400

    change route paths and import genres routes into index

--- my-first-app/vidly/index.js
+++ my-first-app/vidly/index.js
@@ -1,18 +1,14 @@
 const Joi = require('@hapi/joi');
 const express = require('express');
-
-const app = express();
-app.use(express.json());
+const genres = require('./routes/genres')
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`listening on port ${port}...`));
 
+const app = express();
+app.use(express.json());
+app.use('/api/genres', genres);
 
-const genres = [
-    { id: 1, name: 'Action' },
-    { id: 2, name: 'Horror' },
-    { id: 3, name: 'Romance' },
-];
 
 //middleware(JOI), used for data validation
 function validateGenre(genre) {
@@ -24,54 +20,3 @@ function validateGenre(genre) {
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 });
-
-//GET all genres: don't need id or other data
-app.get('/api/genres', (req,res) => {
-    res.send(genres)
-})
-
-//GET a single genre: find by id
-app.get('/api/genres/:id', (req, res) => {
-    // console.log(req.params); === { id: 1}       
-    const genre = genres.find((g)=> g.id === parseInt(req.params.id) );
-    if(!genre) res.status(404).send('That genre ID could not be found.');
-    res.send(genre);
-})
-
-//CREATE/POST a new genre: no id, only what you're creating
-app.post('/api/genres/', (req, res) => {
-    //by default this would be `undefined`, unless using body parsing middleware    
-    const { error } = validateGenre(req.body);    
-    if(error) return res.status(404).send(error.details[0].message);
-    //create a genre object to pushh to our array. With (req.body.name)
-    const genre = {d: genres.length + 1,ame: req.body.name}
-    genres.push(genre)
-    res.send(genres)
-})
-
-//UPDATE/PUT a post genre: need the id and the info you're changing
-// EXPRESS, (req.params): contains the properties of the objext we gave it
-app.put('/api/genres/:id', (req,res)=>{
-    const genre = genres.find((g)=> g.id === parseInt(req.params.id) );
-    const { error } = validateGenre(req.body)
-    //IF GENRE DOESN'T EXIST
-    if (!genre) return res.status(404).send("That genre ID could not be found.");
-    //IF REQUEST IS INVALID
-    if (error) return res.status(404).send(error.details[0].message);
-    //UPDATE/EDIT THE GENRE NAME
-    genre.name = req.body.name
-    //send back to user, singular & updated genre
-    res.send(genre)
-});
-
-//DELETE
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find((g) => g.id === parseInt(req.params.id));
-    //VALIDATE that that genre exists
-    if (!genre) return res.status(404).send("That genre ID could not be found.");
-    //FIND OBJECT IN ARRAY THAT MATCHES GENRE ID
-    const index = genres.indexOf(genre)
-    //REMOVE OBJECT FROM ARRAY
-    genres.splice(index, 1)
-    res.send(genres)
-})
--- my-first-app/vidly/routes/genres.js
+++ my-first-app/vidly/routes/genres.js
@@ -1,6 +1,61 @@
 const express = require('express');
 const router = express.Route();
 
+const genres = [
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Horror' },
+    { id: 3, name: 'Romance' },
+];
 
+//GET all genres: don't need id or other data
+router.get('/', (req, res) => {
+    res.send(genres)
+})
+
+//GET a single genre: find by id
+router.get('/:id', (req, res) => {
+    // console.log(req.params); === { id: 1}       
+    const genre = genres.find((g) => g.id === parseInt(req.params.id));
+    if (!genre) res.status(404).send('That genre ID could not be found.');
+    res.send(genre);
+})
+
+//CREATE/POST a new genre: no id, only what you're creating
+router.post('/', (req, res) => {
+    //by default this would be `undefined`, unless using body parsing middleware    
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+    //create a genre object to pushh to our array. With (req.body.name)
+    const genre = { d: genres.length + 1, ame: req.body.name }
+    genres.push(genre)
+    res.send(genres)
+})
+
+//UPDATE/PUT a post genre: need the id and the info you're changing
+// EXPRESS, (req.params): contains the properties of the objext we gave it
+router.put('/:id', (req, res) => {
+    const genre = genres.find((g) => g.id === parseInt(req.params.id));
+    const { error } = validateGenre(req.body)
+    //IF GENRE DOESN'T EXIST
+    if (!genre) return res.status(404).send("That genre ID could not be found.");
+    //IF REQUEST IS INVALID
+    if (error) return res.status(404).send(error.details[0].message);
+    //UPDATE/EDIT THE GENRE NAME
+    genre.name = req.body.name
+    //send back to user, singular & updated genre
+    res.send(genre)
+});
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    const genre = genres.find((g) => g.id === parseInt(req.params.id));
+    //VALIDATE that that genre exists
+    if (!genre) return res.status(404).send("That genre ID could not be found.");
+    //FIND OBJECT IN ARRAY THAT MATCHES GENRE ID
+    const index = genres.indexOf(genre)
+    //REMOVE OBJECT FROM ARRAY
+    genres.splice(index, 1)
+    res.send(genres)
+})
 
 module.exports = router
