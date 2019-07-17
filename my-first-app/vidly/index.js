4209e69b832bfcb2ded390cc5404a401c05fe72a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-17 19:28:27 -0400

    finish restructuring app routes

--- my-first-app/vidly/index.js
+++ my-first-app/vidly/index.js
@@ -1,22 +1,10 @@
 const Joi = require('@hapi/joi');
 const express = require('express');
 const genres = require('./routes/genres')
-
-const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`listening on port ${port}...`));
-
 const app = express();
+
 app.use(express.json());
 app.use('/api/genres', genres);
 
-
-//middleware(JOI), used for data validation
-function validateGenre(genre) {
-    const schema = {name: Joi.string().min(3).required()}
-    return Joi.validate(genre, schema)
-}
-
-// root of app, check if functioning
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-});
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`listening on port ${port}...`));
--- my-first-app/vidly/routes/genres.js
+++ my-first-app/vidly/routes/genres.js
@@ -58,4 +58,10 @@ router.delete('/:id', (req, res) => {
     res.send(genres)
 })
 
+//middleware(JOI), used for data validation
+function validateGenre(genre) {
+    const schema = { name: Joi.string().min(3).required() }
+    return Joi.validate(genre, schema)
+}
+
 module.exports = router
