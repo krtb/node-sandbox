f2a110cf0495ecbb716e1361eaf0301737477a0d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-17 20:28:08 -0400

    callbacks, promises, async/await to deal with async code

--- async-demo/index.js
+++ async-demo/index.js
@@ -3,8 +3,12 @@ console.log('BEFORE');
 const user = getUser(1)
 
 console.log(user)
-
 console.log('AFTER');
+
+//TODO: 3 patterns for dealing with async code
+// Callbacks
+// Promises
+// Async/Await
 
 function getUser(id) {
     //in this func, need to return a user object
