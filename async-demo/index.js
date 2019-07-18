19660ddd8a78959b93b3c30d36a0e021dcb46475
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-17 20:26:26 -0400

    example of undefined because of async code

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,20 @@
 console.log('BEFORE');
-setTimeout(()=>{
-    console.log('Reaing a user froma a database....')
-}, 2000)
+
+const user = getUser(1)
+
+console.log(user)
+
 console.log('AFTER');
+
+function getUser(id) {
+    //in this func, need to return a user object
+    setTimeout(() => {
+        console.log('Reaing a user from a database....')
+        return {
+            id: id,
+            gitHubUserName: "krtb"
+        }
+    }, 2000)
+
+    return 1
+}
