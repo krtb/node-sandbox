b26f4bbb12d38160a237ab2e2f45d1fb0c84b7aa
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-17 21:51:44 -0400

    adding promises to functions

--- async-demo/index.js
+++ async-demo/index.js
@@ -8,24 +8,34 @@ function getRepos(user) {
     getRepos(user.gitHubUserName, getCommits)
 }
 
-function getCommits(repos) {
-    getCommits(repo, displayCommits) //not calling func, but a reference to it
-}
-
 function displayCommits(commits) {
     console.log(commits);
 }
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database....')
-        callback({id: id,gitHubUserName: "krtb"})
-    }, 2000)
+function getUser(id) {
+    return new Promise((resolve, reject)=>{
+        //kickoff async work
+        setTimeout(() => {
+            console.log('Reading a user from a database....')
+            resolve({ id: id, gitHubUserName: "krtb" }) //call it resolve to return object to user
+        }, 2000)
+    })
 }
 
-function getRepos(username, callback) {
-    setTimeout(()=>{
-        console.log('Grabbing Repos....')
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2000)
+function getRepos(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Grabbing Repos....')
+            resolve(['repo1', 'repo2', 'repo3'])
+        }, 2000)
+    })
 }
+
+function getCommits(repos) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling github API...')
+            resolve(['commit'])
+        }, 2000)
+    })}
+
