e8bba482a6940c2440e506e443fef62b36cb9dde
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-19 17:21:33 -0400

    create 2 async functions

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,7 +1,17 @@
-const p = Promise.reject(new Error('reason for rejection'));
-p.catch(err => console.log(err) )
+// RUNNING PARALLEL PROMISES
 
+// simulation for calling facebook api
+const p1 = new Promise((resolve) => {
+    setTimeout(()=>{
+        console.log('Async operation 1...');
+        resolve(1)
+    }, 2000)
+})
 
-
-//ERROR
-//using native error objectg will include the callstack
+// simulation for calling a second api, let's say instagram
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 2...');
+        resolve(2)
+    }, 2000)
+})
