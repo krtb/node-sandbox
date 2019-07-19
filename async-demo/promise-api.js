35cb1326ccc905af88881e91b38a7151743ea4b9
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-19 17:16:35 -0400

    when using a catch, include callstack

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,2 +1,7 @@
-const p = Promise.resolve({id:1});
-p.then(result => console.log(result) )
+const p = Promise.reject(new Error('reason for rejection'));
+p.catch(err => console.log(err) )
+
+
+
+//ERROR
+//using native error objectg will include the callstack
