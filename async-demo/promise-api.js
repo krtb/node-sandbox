f6e3bd331b4b11bd78787191274bffba957c6ccb
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-19 17:26:24 -0400

    use the all method on class of Promise

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -15,3 +15,11 @@ const p2 = new Promise((resolve) => {
         resolve(2)
     }, 2000)
 })
+
+// kick off both, and when they comnplete, want to do something after
+
+// .all is another method that belongs to promise class, instead of promise object
+Promise.all([p1,p2]) // give it an array of promises, returns a new promise when all promises in this array resolved
+.then(result => console.log(result))
+
+// still don't have concurrency, a single thread is kicking off both ALMOST at the same time
