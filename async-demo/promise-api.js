afa41bb0847e74fc97fb67b900e44819a2cc2c02
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-19 17:31:20 -0400

    reject promise to show that still resolves at the same time

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,10 +1,10 @@
 // RUNNING PARALLEL PROMISES
 
 // simulation for calling facebook api
-const p1 = new Promise((resolve) => {
+const p1 = new Promise((resolve, reject) => {
     setTimeout(()=>{
         console.log('Async operation 1...');
-        resolve(1)
+        reject(new Error('because something failed...'))
     }, 2000)
 })
 
@@ -19,7 +19,10 @@ const p2 = new Promise((resolve) => {
 // kick off both, and when they comnplete, want to do something after
 
 // .all is another method that belongs to promise class, instead of promise object
-Promise.all([p1,p2]) // give it an array of promises, returns a new promise when all promises in this array resolved
+// give it an array of promises, returns a new promise when all promises in this array resolved
+Promise.all([p1,p2]) 
 .then(result => console.log(result))
+.catch(err => console.log('Error', err.message))
 
 // still don't have concurrency, a single thread is kicking off both ALMOST at the same time
+// both async operations are started almost at the same time
