6498d5581070819c9582c6326fd7aaeb412ed8bd
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-19 18:24:08 -0400

    use promise.race when you need a specific promise to complete and then do something

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -4,7 +4,7 @@
 const p1 = new Promise((resolve, reject) => {
     setTimeout(()=>{
         console.log('Async operation 1...');
-        reject(new Error('because something failed...'))
+        reject(1)
     }, 2000)
 })
 
@@ -16,14 +16,6 @@ const p2 = new Promise((resolve) => {
     }, 2000)
 })
 
-// kick off both, and when they comnplete, want to do something after
-
-// .all is another method that belongs to promise class, instead of promise object
-// give it an array of promises, returns a new promise when all promises in this array resolved
-Promise.all([p1,p2]) 
+Promise.race([p1,p2]) 
 .then(result => console.log(result))
 .catch(err => console.log('Error', err.message))
-
-// still don't have concurrency, a single thread is kicking off both ALMOST at the same time
-// both async operations are started almost at the same time
-// if any of our promises are rejected, final promise will be considered rejected
