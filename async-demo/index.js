721e72544434c3dc3e9bb8b76f51e8daa8e94dc7
Kurt Bauer <kurtbauer55@protonmail.com>
2019-07-19 18:36:34 -0400

    refactor with async and await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,13 +1,25 @@
 console.log('BEFORE');
 
-getUser(1).then(user => getRepos(user.gitHubUserName)).then(repos => getCommits(repos[0])).then(commits => console.log('Commits', commits) )
-.catch(err => console.logI('Error', err.message)) 
+//promise based approach
+getUser(1)
+    .then(user => getRepos(user.gitHubUserName))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits', commits) )
+    .catch(err => console.logI('Error', err.message)) 
 
-console.log('AFTER');
+//async and await: with this can write async code that looks sync
+const user = await getUser(1); // anytime you're running a function that returns a promise, can await the result
+const repos = await getRepos(user.gitHubUserName);
+const commits = await getCommits(repos[0]);
+console.log(commits);
+
+
 
 function getRepos(user) {
     getRepos(user.gitHubUserName, getCommits)
 }
+
+console.log('AFTER');
 
 function displayCommits(commits) {
     console.log(commits);
