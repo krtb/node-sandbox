77e57a3d969a4fc95ee8a50a59d0395faa93f978
Kurt Bauer <kurtbauer55@protonmail.com>
2019-08-21 14:12:04 -0400

    change functions to emit info to terminal

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,58 +1,65 @@
-console.log('BEFORE');
+console.log('Before');
 
-//promise based approach
-getUser(1)
-    // .then(user => getRepos(user.gitHubUserName))
-    // .then(repos => getCommits(repos[0]))
-    // .then(commits => console.log('Commits', commits) )
-    // .catch(err => console.logI('Error', err.message)) 
+// Callback-based approach
+//
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     })
+//   })
+// });
 
-//async and await: with this can write async code that looks sync
+
+// Promise-based approach
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commits', commits))
+//   .catch(err => console.log('Error', err.message));
+
+
+// Async and Await approach
 async function displayCommits() {
-    // when using await, have to decorate your function with the async modifier, which is just syntactical sugar
-    const user = await getUser(1); // anytime you're running a function that returns a promise, can await the result
-    // when result of await finished, will release our single thread, and make it available to do other things
-    // then store ther result in the USER const, and move on to the next line
-    const repos = await getRepos(user.gitHubUserName);
-    const commits = await getCommits(repos[0]);
-    console.log(commits);
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
 }
-displayCommits()
-// console.log('AFTER');
+displayCommits();
 
-function getRepos(user) {
-    getRepos(user.gitHubUserName, getCommits)
-}
-
-
-function displayCommits(commits) {
-    console.log(commits);
-}
+console.log('After');
 
 function getUser(id) {
-    return new Promise((resolve, reject)=>{
-        //kickoff async work
+    return new Promise((resolve, reject) => {
+        // Kick off some async work 
         setTimeout(() => {
-            console.log('Reading a user from a database....')
-            resolve({ id: id, gitHubUserName: "krtb" }) //call it resolve to return object to user
-        }, 2000)
-    })
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'mosh' });
+        }, 2000);
+    });
 }
 
-function getRepos(username) {
+function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('Grabbing Repos....')
-            resolve(['repo1', 'repo2', 'repo3'])
-        }, 2000)
-    })
+            console.log('Calling GitHub API...');
+            // resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error('Could not get the repos.'));
+        }, 2000);
+    });
 }
 
-function getCommits(repos) {
+function getCommits(repo) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('Calling github API...')
-            resolve(['commit'])
-        }, 2000)
-    })}
-
+            console.log('Calling GitHub API...');
+            resolve(['commit']);
+        }, 2000);
+    });
+}
