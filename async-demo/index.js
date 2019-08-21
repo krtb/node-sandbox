c2d5233ac147afd67aa157eaf21dedc83e893b6b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-08-21 14:10:46 -0400

    add funcs using async and await

--- async-demo/index.js
+++ async-demo/index.js
@@ -2,24 +2,28 @@ console.log('BEFORE');
 
 //promise based approach
 getUser(1)
-    .then(user => getRepos(user.gitHubUserName))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits) )
-    .catch(err => console.logI('Error', err.message)) 
+    // .then(user => getRepos(user.gitHubUserName))
+    // .then(repos => getCommits(repos[0]))
+    // .then(commits => console.log('Commits', commits) )
+    // .catch(err => console.logI('Error', err.message)) 
 
 //async and await: with this can write async code that looks sync
-const user = await getUser(1); // anytime you're running a function that returns a promise, can await the result
-const repos = await getRepos(user.gitHubUserName);
-const commits = await getCommits(repos[0]);
-console.log(commits);
-
-
+async function displayCommits() {
+    // when using await, have to decorate your function with the async modifier, which is just syntactical sugar
+    const user = await getUser(1); // anytime you're running a function that returns a promise, can await the result
+    // when result of await finished, will release our single thread, and make it available to do other things
+    // then store ther result in the USER const, and move on to the next line
+    const repos = await getRepos(user.gitHubUserName);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+}
+displayCommits()
+// console.log('AFTER');
 
 function getRepos(user) {
     getRepos(user.gitHubUserName, getCommits)
 }
 
-console.log('AFTER');
 
 function displayCommits(commits) {
     console.log(commits);
