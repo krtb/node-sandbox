c61c69d2bbb923c3d870fc2cd8fb0d18117a57bc
Kurt Bauer <kurtbauer55@protonmail.com>
2019-08-28 14:57:48 -0400

    created a new course schema, only part of mongoose

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 //add name of DB, mongo auto adds this name for us
 // in reall app, would be better to use debug module instead of console, more constrol over debugging messages
 mongoose.connect('mongodb://localhost/playground') 
-.then(()=> console.log('Connected to MongoDB'))
+    .then(()=> console.log('Connected to MongoDB'))
     .catch((err) => console.log('Could not connect to MongoDB', err))
+
+const courseSchema = new mongoose.Schema({
+
+});
 
