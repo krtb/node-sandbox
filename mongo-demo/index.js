9d2fd0962e81ebd6eee737ebacb5e00875fa75ce
Kurt Bauer <kurtbauer55@protonmail.com>
2019-08-28 14:54:50 -0400

    success connecting to mongdoDB

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost')
+//in real app, conection string would come from a config file
+//add name of DB, mongo auto adds this name for us
+// in reall app, would be better to use debug module instead of console, more constrol over debugging messages
+mongoose.connect('mongodb://localhost/playground') 
+.then(()=> console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Could not connect to MongoDB', err))
+
