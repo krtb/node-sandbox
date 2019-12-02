7ac2d2fd0b99fcc1a6b2674c44781d067cac1063
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-02 18:50:35 -0500

    use node to create and write to file

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,1 +1,5 @@
+// import Node, file system package
+const fs = require('fs')
+
+// using writeFile and writeFileSync methods. Takes in 2 args, file and data to write to file
 fs.writeFileSync('notes.txt', 'This file was created by Node.js')
